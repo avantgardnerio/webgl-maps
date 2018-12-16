5d0798673e9ccc2494088c1f2d1ee7cecbffd7df
Brent Gardner <bgardner@squarelabs.net>
2018-12-16 15:45:13 -0700

    Formatting

--- js/index.mjs
+++ js/index.mjs
@@ -23,63 +23,40 @@ window.onload = async () => {
             modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
         },
     };
-
-    // Here's where we call the routine that builds all the
-    // objects we'll be drawing.
     const buffers = initBuffers(gl);
-
-    // Draw the scene
     drawScene(gl, programInfo, buffers);
 }
 
-// Initialize a shader program, so WebGL knows how to draw our data
 const initShaderProgram = (gl, vsSource, fsSource) => {
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-    // Create the shader program
     const shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
 
-    // If creating the shader program failed, alert
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
-        return null;
+        const info = gl.getProgramInfoLog(shaderProgram);
+        throw new Error(`Unable to initialize the shader program: ${info}`);
     }
-
     return shaderProgram;
 }
 
-// creates a shader of the given type, uploads the source and compiles it.
 const loadShader = (gl, type, source) => {
     const shader = gl.createShader(type);
-
-    // Send the source to the shader object
     gl.shaderSource(shader, source);
-
-    // Compile the shader program
     gl.compileShader(shader);
-
-    // See if it compiled successfully
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
         gl.deleteShader(shader);
-        return null;
+        const info = gl.getShaderInfoLog(shader);
+        throw new Error(`An error occurred compiling the shaders: ${info}`)
     }
-
     return shader;
 }
 
 const initBuffers = (gl) => {
-    // Create a buffer for the square's positions.
     const positionBuffer = gl.createBuffer();
-
-    // Select the positionBuffer as the one to apply buffer operations to from here out.
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-    // Now create an array of positions for the square.
     const positions = [
         -1.0, -1.0,
         1.0, 1.0,
@@ -89,13 +66,11 @@ const initBuffers = (gl) => {
         1.0, -1.0,
         -1.0, -1.0
     ];
-
-    // Now pass the list of positions into WebGL to build the shape
-    gl.bufferData(gl.ARRAY_BUFFER,
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
         new Float32Array(positions),
         gl.STATIC_DRAW
     );
-
     return { position: positionBuffer };
 }
 
@@ -157,6 +132,6 @@ const drawScene = (gl, programInfo, buffers) => {
     );
 
     const vertexCount = 6;
-    gl.drawArrays(gl.TRIANGLES, offset, vertexCount);
+    gl.drawArrays(gl.LINE_STRIP, offset, vertexCount);
 }
 
