1f33ba79e5657fb0f03e3004083989c9d85437f4
Brent Gardner <bgardner@squarelabs.net>
2018-12-16 11:57:30 -0700

    Add a canvas

--- js/index.mjs
+++ js/index.mjs
@@ -1,3 +1,11 @@
 window.onload = () => {
-    alert('Hello world!');
+    // https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL
+    const cnvMain = document.createElement(`canvas`);
+    cnvMain.setAttribute(`width`, `640`);
+    cnvMain.setAttribute(`height`, `480`);
+    document.body.appendChild(cnvMain);
+
+    const gl = cnvMain.getContext(`webgl`);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
