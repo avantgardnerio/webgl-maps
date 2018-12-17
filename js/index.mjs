12a6c114876c8b09e5c63a037970e2e6c3039476
Brent Gardner <bgardner@squarelabs.net>
2018-12-17 16:20:13 -0700

    Reorg

--- js/index.mjs
+++ js/index.mjs
@@ -58,6 +58,37 @@ const initBuffers = (gl) => {
         -t,  0,  1,
     ];
 
+    // Face indices
+    const indices = [
+        // 5 faces around point 0
+        0, 11, 5,
+        0, 5, 1,
+        0, 1, 7,
+        0, 7, 10,
+        0, 10, 11,
+
+        // 5 adjacent faces
+        1, 5, 9,
+        5, 11, 4,
+        11, 10, 2,
+        10, 7, 6,
+        7, 1, 8,
+
+        // 5 faces around point 3
+        3, 9, 4,
+        3, 4, 2,
+        3, 2, 6,
+        3, 6, 8,
+        3, 8, 9,
+
+        // 5 adjacent faces
+        4, 9, 5,
+        2, 4, 11,
+        6, 2, 10,
+        8, 6, 7,
+        9, 8, 1,
+    ];
+
     // normals
     const vertexNormals = [
         // Front
@@ -129,37 +160,6 @@ const initBuffers = (gl) => {
         1.0, 0.0,
         1.0, 1.0,
         0.0, 1.0,
-    ];
-
-    // Face indices
-    const indices = [
-        // 5 faces around point 0
-        0, 11, 5,
-        0, 5, 1,
-        0, 1, 7,
-        0, 7, 10,
-        0, 10, 11,
-
-        // 5 adjacent faces
-        1, 5, 9,
-        5, 11, 4,
-        11, 10, 2,
-        10, 7, 6,
-        7, 1, 8,
-
-        // 5 faces around point 3
-        3, 9, 4,
-        3, 4, 2,
-        3, 2, 6,
-        3, 6, 8,
-        3, 8, 9,
-
-        // 5 adjacent faces
-        4, 9, 5,
-        2, 4, 11,
-        6, 2, 10,
-        8, 6, 7,
-        9, 8, 1,
     ];
 
     const positionBuffer = gl.createBuffer();
