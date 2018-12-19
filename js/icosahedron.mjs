94a3cbef4009312f1b0ba2067d31d779da676858
Brent Gardner <bgardner@squarelabs.net>
2018-12-18 18:32:12 -0700

    Refactor

--- js/icosahedron.mjs
+++ js/icosahedron.mjs
@@ -1,7 +1,8 @@
 import {normalize, getMidPoint} from './utils.mjs';
+import {tileToRad} from './utils.mjs';
 
 // http://blog.andreaskahler.com/2009/06/creating-icosphere-mesh-in-code.html
-export const initBuffers = (gl) => {
+export const initBuffers = (gl, tilex, tiley, zoom) => {
     // create 12 vertices of a icosahedron
     const t = (1.0 + Math.sqrt(5.0)) / 2.0;
     const positions = [
@@ -21,38 +22,8 @@ export const initBuffers = (gl) => {
         ...normalize([-t, 0, 1]),
     ];
 
-    // Face indices
-    let indices = [
-        // 5 faces around point 0
-        0, 11, 5,
-        0, 5, 1,
-        0, 1, 7,
-        0, 7, 10,
-        0, 10, 11,
-
-        // 5 adjacent faces
-        1, 5, 9,
-        5, 11, 4,
-        11, 10, 2,
-        10, 7, 6,
-        7, 1, 8,
-
-        // 5 faces around point 3
-        3, 9, 4,
-        3, 4, 2,
-        3, 2, 6,
-        3, 6, 8,
-        3, 8, 9,
-
-        // 5 adjacent faces
-        4, 9, 5,
-        2, 4, 11,
-        6, 2, 10,
-        8, 6, 7,
-        9, 8, 1,
-    ];
-
     // refine mesh
+    let indices = [...INITIAL_INDICES];
     for (let detail = 0; detail < 3; detail++) {
         const newIndices = [];
         for (let i = 0; i < indices.length; i += 3) {
@@ -133,3 +104,34 @@ export const initBuffers = (gl) => {
         indexCount: indices.length
     };
 };
+
+// Face indices
+const INITIAL_INDICES = [
+    // 5 faces around point 0
+    0, 11, 5,
+    0, 5, 1,
+    0, 1, 7,
+    0, 7, 10,
+    0, 10, 11,
+
+    // 5 adjacent faces
+    1, 5, 9,
+    5, 11, 4,
+    11, 10, 2,
+    10, 7, 6,
+    7, 1, 8,
+
+    // 5 faces around point 3
+    3, 9, 4,
+    3, 4, 2,
+    3, 2, 6,
+    3, 6, 8,
+    3, 8, 9,
+
+    // 5 adjacent faces
+    4, 9, 5,
+    2, 4, 11,
+    6, 2, 10,
+    8, 6, 7,
+    9, 8, 1,
+];
