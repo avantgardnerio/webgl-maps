de9162503aaef6983045b5c6d232ffef54895fd9
Brent Gardner <bgardner@squarelabs.net>
2018-12-20 09:21:32 -0700

    Fix wrapping

--- js/texture.mjs
+++ js/texture.mjs
@@ -13,14 +13,14 @@ export const loadTexture = (gl, url) => {
     image.onload = function () {
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-        if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-            gl.generateMipmap(gl.TEXTURE_2D); // Yes, it's a power of 2. Generate mips.
-        } else {
+        // if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+        //     gl.generateMipmap(gl.TEXTURE_2D); // Yes, it's a power of 2. Generate mips.
+        // } else {
             // No, it's not a power of 2. Turn of mips and set wrapping to clamp to edge
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-        }
+        // }
     };
     image.src = url;
 
