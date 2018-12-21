a770cd2732a19be23c7c1efa6b65e549bc1df199
Brent Gardner <bgardner@squarelabs.net>
2018-12-20 20:59:33 -0700

    TODO

--- public/js/index.mjs
+++ public/js/index.mjs
@@ -72,6 +72,7 @@ onload = async () => {
             }
 
             if(zoom < 18 && (zoom < 2 || width > TILE_SIZE || height > TILE_SIZE)) {
+                // TODO: also ensure on screen
                 getTiles(zoom + 1, tileX * 2, tileY * 2, mat, tiles);
                 getTiles(zoom + 1, tileX * 2 + 1, tileY * 2, mat, tiles);
                 getTiles(zoom + 1, tileX * 2, tileY * 2 + 1, mat, tiles);
